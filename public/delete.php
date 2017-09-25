932731f84640e7b5928da1e40898cc5e6bf1e695
Aldarien <aldarien85@gmail.com>
2017-09-24 21:09:33 -0300

    Not saving removed version

--- public/delete.php
+++ public/delete.php
@@ -4,7 +4,7 @@ include_once dirname(__DIR__) . '/bootstrap/autoload.php';
 if (isset($_POST['link'])) {
 	$links = new Links\Links();
 	$key = $_POST['link'];
-	if ($links->remove($key)) {
+	if ($links->remove($key) and $links->saveFile()) {
 		header('Location: .');
 	} else {
 		throw new Exception('Link not found.');
